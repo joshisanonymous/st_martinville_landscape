2c987518e9e283d36d71f20ba61cb9231b7d3b02
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2024-03-21 18:09:38 -0400

    More progress

--- scripts/maps.R
+++ scripts/maps.R
@@ -4,13 +4,34 @@ library(osmdata)
 # Map variables----------------------------------------------------------------
 sign_size <- 2
 sign_colors <- c("#619CFF", "#00BA38", "#F8766D")
-town <- opq(getbb("St. Martinville"))
-streets <- osmdata_sf(add_osm_feature(town, key = c("highway")))
-water <- osmdata_sf(add_osm_feature(town, key = c("water")))
-historic_dist <- data.frame(
-  x = c(-93.81760416553334, -93.81760416553334, -89.6886555668733, -89.6886555668733),
-  y = c(29.24004503400145, 32.81866418073393, 32.81866418073393, 29.24004503400145)
-)
+
+# Louisiana
+louisiana <- opq(getbb("Louisiana")) |>
+  add_osm_feature(key = "boundary", value = "administrative") |>
+  add_osm_feature(key = "admin_level", value = c("4", "6")) |>
+  osmdata_sf()
+state <- subset(louisiana$osm_multipolygons,
+                admin_level == "4" & name == "Louisiana")
+parishes <- louisiana$osm_multipolygons |>
+  st_filter(state, .predicate=st_within) |>
+  subset(admin_level == 6)
+
+# St Martinville
+stmartinville <- opq(getbb("St. Martinville"))
+city <- osmdata_sf(add_osm_feature(stmartinville, key = "admin_level", value = "8"))
+streets_foot <- osmdata_sf(add_osm_feature(
+  stmartinville, key = "highway", value = c("footway","track","path")))
+streets_reg <- osmdata_sf(add_osm_feature(
+  stmartinville, key = "highway", value = c("residential","tertiary", "secondary",
+                                            "tertiary_link","secondary_link",
+                                            "primary_link","trunk_link")))
+streets_main <- osmdata_sf(add_osm_feature(
+  stmartinville, key = "highway", value = c("primary","trunk")))
+water <- osmdata_sf(add_osm_feature(stmartinville, key = "water"))
+# historic_dist <- data.frame(
+#   x = c(-93.81760416553334, -93.81760416553334, -89.6886555668733, -89.6886555668733),
+#   y = c(29.24004503400145, 32.81866418073393, 32.81866418073393, 29.24004503400145)
+# )
 
 # Subsets
 signs_english <- signs[signs$Language == "English", ]
@@ -18,28 +39,26 @@ signs_french <- signs[signs$Language == "French", ]
 signs_bilingual <- signs[signs$Language == "French-English", ]
 
 # Maps ------------------------------------------------------------------------
-map <- ggplot() +
+mapsm <- ggplot() +
+  geom_sf(data = city$osm_multipolygons,
+          fill = "lightyellow",
+          color = "yellow") +
   geom_sf(data = water$osm_multipolygons,
           inherit.aes = FALSE,
           fill = "steelblue") +
-  geom_sf(data = streets$osm_lines %>%
-            filter(highway %in% c("residential","tertiary", "secondary",
-                                  "tertiary_link","secondary_link",
-                                  "primary_link","trunk_link")),
-          inherit.aes = FALSE,
-          linewidth = 2,
-          color = "darkgrey") +
-  geom_sf(data = streets$osm_lines %>%
-            filter(highway %in% c("footway","track","path")),
+  geom_sf(data = streets_foot$osm_lines,
           inherit.aes = FALSE,
           linewidth = 1,
           color = "lightgrey") +
-  geom_sf(data = streets$osm_lines %>%
-            filter(highway %in% c("primary","trunk")),
+  geom_sf(data = streets_reg$osm_lines,
+          inherit.aes = FALSE,
+          linewidth = 2,
+          color = "darkgrey") +
+  geom_sf(data = streets_main$osm_lines,
           inherit.aes = FALSE,
           linewidth = 3,
           color = "orange") +
-  geom_polygon(data = historic_dist, aes(x = x, y = y, fill = "pink")) +
+  # geom_polygon(data = historic_dist, aes(x = x, y = y, fill = "pink")) +
   geom_point(data = signs_english,
              mapping = aes(x = GPSLongitude,
                            y = GPSLatitude),
@@ -64,10 +83,23 @@ map <- ggplot() +
              shape = 21,
              inherit.aes = FALSE,
              size = sign_size) +
+  theme_void()
+
+mapsm_norm <- mapsm +
+  geom_textbox(aes(label = "Saint Martinville, LA"),
+               y = 30.138, x = -91.838, size = 6,
+               width = unit(2.25, "inch"),
+               fill = "lightgreen") +
   coord_sf(ylim = c(30.10, 30.15),
            xlim = c(-91.85, -91.80),
-           expand = FALSE) +
-  geom_text(aes(label = "Saint Martinville,\nLousiana"),
-            y = 30.127, x = -91.81, size = 10) +
+           expand = FALSE)
+
+mapsm_dists <- mapsm +
+  coord_sf(ylim = c(30.117, 30.129),
+           xlim = c(-91.836, -91.821),
+           expand = FALSE)
+
+mapla <- ggplot() +
+  geom_sf(data = state) +
+  geom_sf(data = parishes) +
   theme_void()
-map
